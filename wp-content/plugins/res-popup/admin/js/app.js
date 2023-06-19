c0a30df0e2636c915752067af14321bd9cf6038f
jjbaldizon16 <baldizon.cv16@gmail.com>
2023-06-18 18:55:25 -0600

    update
    update

---
+++ wp-content/plugins/res-popup/admin/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Archivo js de la parte de la administracion
+ */
+
+$ = jQuery.noConflict();
+
+$(document).ready(function(){
+
+   alert('Ventana de prueba');
+
+})
--- wp-content/plugins/res-popup/partials/res-archivos.php
+++ wp-content/plugins/res-popup/partials/res-archivos.php
@@ -36,16 +36,78 @@
 
  wp_enqueue_style(
 
-    'bootstrap-rtl.css',
-    plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/css/bootstrap.min.css',
+    'bootstrap-rtl-css',
+    plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/css/bootstrap.rtl.min.css',
     [],
     '5.3.0',
     'all'
 
  );
 
+ wp_enqueue_style(
+
+  'bootstrap-grid-css',
+  plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/css/bootstrap.grid.min.css',
+  [],
+  '5.3.0',
+  'all'
+
+);
+
+wp_enqueue_style(
+
+  'bootstrap-grid-rtl-css',
+  plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/css/bootstrap.grid.rtl.min.css',
+  [],
+  '5.3.0',
+  'all'
+
+);
+
+wp_enqueue_style(
+
+  'bootstrap-reboot-css',
+  plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/css/bootstrap-reboot.min.css',
+  [],
+  '5.3.0',
+  'all'
+
+);
+
+wp_enqueue_style(
+
+  'bootstrap-reboot-rtl-css',
+  plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/css/bootstrap-reboot.rtl.min.css',
+  [],
+  '5.3.0',
+  'all'
+
+);
+
+wp_enqueue_style(
+
+  'bootstrap-utilities-css',
+  plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/css/bootstrap-utilities.min.css',
+  [],
+  '5.3.0',
+  'all'
+
+);
+
+wp_enqueue_style(
+
+  'bootstrap-utilities-rtl-css',
+  plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/css/bootstrap-utilities.rtl.min.css',
+  [],
+  '5.3.0',
+  'all'
+
+);
+
 
  }
+
+ add_action( 'admin_enqueue_scripts', 'enqueue_style' );
 
  function enqueue_scripts( $hook ){
 
@@ -54,6 +116,34 @@
       return;
  
     }
+
+    //Encolando js
+
+    wp_enqueue_script(
+       'admin-script',
+       plugin_dir_url(__DIR__) . 'admin/js/app.js',
+       ['jquery', 'bootstrap-min'],
+       '1.0.0',
+       true     
+    );
+    //Encolando libreria de bootstrap
+    wp_enqueue_script(
+      'bootstrap-min',
+      plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/js/bootstrap.min.js',
+      ['jquery'],
+      '5.3.0',
+      true     
+   );
  
+   wp_enqueue_script(
+    'bootstrap-bundle',
+    plugin_dir_url(__DIR__) . 'helpers/bootstrap-5.3.0/js/bootstrap.bundle.min.js',
+    ['jquery'],
+    '5.3.0',
+    true     
+ );
+
   }
+
+  add_action( 'admin_enqueue_scripts', 'enqueue_scripts' );
 
