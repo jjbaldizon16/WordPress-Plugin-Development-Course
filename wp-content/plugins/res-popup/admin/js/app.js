f9d3292bd0f0ce9061d88676edd7a529dc3bffee
jjbaldizon16 <baldizon.cv16@gmail.com>
2023-06-19 03:22:44 -0600

    update
    update

--- wp-content/plugins/res-popup/admin/js/app.js
+++ wp-content/plugins/res-popup/admin/js/app.js
@@ -6,6 +6,14 @@ $ = jQuery.noConflict();
 
 $(document).ready(function(){
 
-   
+    $('#btn_crear').on('click', function(){
+
+        var Modalpopup = new bootstrap.Modal(document.getElementById('Modalpopup'), {
+            keyboard: false
+          })
+
+        Modalpopup.show();
+
+    })
 
 })
--- wp-content/plugins/res-popup/admin/res-display-menu.php
+++ wp-content/plugins/res-popup/admin/res-display-menu.php
@@ -106,7 +106,7 @@
 
                         <!--boton crear-->
                         
-                        <a href="" type="button" class="btn btn-primary text-uppercase btn-crear" id="btn_crear" style="">
+                        <a href="#" type="button" class="btn btn-primary text-uppercase btn-crear" id="btn_crear" style="">
                         
                         <span class="dashicons dashicons-plus"></span>  
                         <span style="margin-top: -4px;">Crear</span>
@@ -122,3 +122,7 @@
        </div>
 
 </div>
+
+
+<?php include 'res-modal.php' ?>
+
